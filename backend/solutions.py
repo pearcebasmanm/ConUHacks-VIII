1a19a256eab1b4cb09276f383902a83305dd7517
Max <pearcebasmanm@gmail.com>
2024-01-20 20:17:14 -0500

    added multiple algorithms

--- backend/solutions.py
+++ backend/solutions.py
@@ -3,45 +3,103 @@ from typing import TypeAlias
 Request: TypeAlias = (str, str, str)
 Schedule: TypeAlias = list[(int, int, Request)]
 
-def solve_day(requests: list[Request]) -> (dict[str, Schedule], list[Request]):
-    bays: dict[str, Schedule]= {
-        "compact": [],
-        "medium": [],
-        "full-size": [],
-        "class 1 truck": [],
-        "class 2 truck": [],
-        "general 1": [],
-        "general 2": [],
-        "general 3": [],
-        "general 4": [],
-        "general 5": []
-    }
+def optimized(requests, bays, rejected) -> None:
+    # sort by time
+    requests.sort(key=lambda request: request[1])
 
-    rejected: list[Request] = []
-
-    requests.sort(key=issued_time)
-
-    
+    # convert to smaller data types
+    minified_requests = []
     for request in requests:
-        (issued, requested, vehicle) = request
-        (start, end) = request_start_end(request)
-        
+        (_, requested, vehicle) = request
+        [_, time] = requested.split()
+        (start, end) = request_start_end(time, vehicle)
+        cost = servicing_charge[vehicle]
+        minified_requests.append((start, end, cost))
+
+    # create compacter versions of the destination structures
+    minified_bays = [[]] * 10
+    minified_rejected = []
+    
+    # optimized on those smaller data types
+    optimized_recursive(0, minified_requests, minified_bays, minified_rejected)
+
+    # convert the results back to actual choices
+    for idx, bay_name in enunmerate(BAY_NAMES):
+        bays[bay_name] = [(start, end, requests[i]) for (start, end, i) in minified_bays[idx]]
+    rejected.entend(requests[i] for i in minified_rejected)
+
+
+def optimized_recursive(index: int, requests: list[(int, int, int)], bays: list[list[(int, int, int)]], rejected: list[int]) -> int:   
+    index = 0
+
+    while True:
+        (start, end, vehicle) = requests[index]
+
+        bays = [BAY_NAMES[idx] for idx, bay in enumerate(bays) if all(other_start not in range(start, end) and other_end not in range(start, end) for (start, end, _) in bay)]
+
+        colliders
+
+        place_found: bool = False
         for schedule in [bays[vehicle], bays["general 1"], bays["general 2"], bays["general 3"], bays["general 4"], bays["general 5"]]:
             conflicting: bool = False
             for time in schedule:
                 if time[0] in range(start, end) or time[1] in range(start, end):
-                    conflicting = true
+                    conflicting = True
                     break
             
             if not conflicting:
                 bays[vehicle].append((start, end, request))
+                place_found = True
                 break
-        else:
+
+        if not place_found:
+            print(request, bays)
+            rejected.append(request);
+    
+
+def reserved_time_chronological(requests, bays, rejected) -> None:
+    requests.sort(key=lambda request: request[1])
+    prioritize_order(requests, bays, rejected)
+
+    
+def issued_time_chronological(requests, bays, rejected) -> None:
+    requests.sort(key=lambda request: request[0])
+    prioritize_order(requests, bays, rejected)
+
+    
+def prioritize_order(requests, bays, rejected) -> None:
+    for request in requests:
+        (_, requested, vehicle) = request
+        [_, time] = requested.split()
+        (start, end) = request_start_end(time, vehicle)
+
+        place_found: bool = False
+        for schedule in [bays[vehicle], bays["general 1"], bays["general 2"], bays["general 3"], bays["general 4"], bays["general 5"]]:
+            conflicting: bool = False
+            for time in schedule:
+                if time[0] in range(start, end) or time[1] in range(start, end):
+                    conflicting = True
+                    break
+            
+            if not conflicting:
+                bays[vehicle].append((start, end, request))
+                place_found = True
+                break
+
+        if not place_found:
+            print(request, bays)
             rejected.append(request);
 
-    return (bays, rejected)
 
+def request_start_end(time: str, vehicle: str) -> (int, int):
+    [hours, minutes] = time.split(":")
+    start = int(hours) * 60 + int(minutes)
+    end = start + servicing_time[vehicle]
+    return (start, end)
 
+VEHICLE_NAMES = ["compact", "medium", "full-size", "class 1 truck", "class 2 truck"]
+
+BAY_NAMES = VEHICLE_NAMES + [f"general {i}" for i in range(1, 6)]
 
 servicing_time = {
     "compact": 30,
@@ -51,19 +109,10 @@ servicing_time = {
     "class 2 truck": 120
 }
 
-def issued_time(request: Request) -> str:
-    return request[0]
-
-def request_start_end(request: Request) -> (int, int):
-    (_, requested_datetime, vehicle) = request
-    requested_time = requested_datetime.split()[1]
-    [hours, minutes] = requested_time.split(":")
-    start = int(hours) * 60 + int(minutes)
-    end = start + servicing_time[vehicle]
-    return (start, end)
-
-def main() -> None:
-    returned = solve_day([("15:00", "2023-01-01 12:40", "compact"), ("08:00", "2023-01-01 12:50", "class 2 truck")])
-    print(returned)
-    
-main()
+servicing_charge = {
+    "compact": 150,
+    "medium": 150,
+    "full-size": 150,
+    "class 1 truck": 250,
+    "class 2 truck": 700
+}
