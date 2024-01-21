63510ae757aeaaea755a8e2cc8aa2884fbc2d329
Abdulrahman M <53632818+Aoof@users.noreply.github.com>
2024-01-21 10:52:45 -0500

    final

--- tire-change-shop/pages/api/data.ts
+++ tire-change-shop/pages/api/data.ts
@@ -2,9 +2,8 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    console.log("API called");
-    const uri = process.env.MONGODB_URI || "mongodb+srv://ADMIN:qwe123qwe123@cluster0.uspsoud.mongodb.net/?retryWrites=true&w=majority";
-    const dbName = process.env.MONGODB_DB || 'Schedule_Optimization';
+    const uri = "mongodb+srv://ADMIN:qwe123qwe123@cluster0.uspsoud.mongodb.net/?retryWrites=true&w=majority";
+    const dbName = 'Schedule_Optimization';
 
     if (!uri || !dbName) {
         res.status(500).json({ message: 'Environment variables MONGODB_URI and MONGODB_DB must be set' });
@@ -18,15 +17,24 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
             deprecationErrors: false
         }
     })
-
+    
     try {
-        console.log("Connecting...")
         await client.connect();
-        console.log("Connected to MongoDB")
+        
         const db = client.db(dbName);
+        let data, date;
 
-        console.log("Connected to MongoDB");
-        const data = await db.collection('ScheduleCollectionName').find({}).toArray();
+        if (req.query.date) {
+            date = req.query.date;
+        }
+        
+        if (!date) {
+            data = await db.collection('ScheduleCollectionName').find().toArray();
+            date = data[0].Datetime_Requested;
+            data = await db.collection('ScheduleCollectionName').find({ Datetime_Requested: { $gte: new Date(date), $lt: new Date(date + 1) } }).toArray();
+        } else {
+            data = await db.collection('ScheduleCollectionName').find({ Datetime_Requested: { $gte: new Date(date), $lt: new Date(date + 1) } }).toArray();
+        }
 
         res.status(200).json(data);
     } catch (error : any) {
